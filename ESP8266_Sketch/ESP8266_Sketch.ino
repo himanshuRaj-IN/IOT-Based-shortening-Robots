9b482e8b0e7e1e8260b84177f8eee03e907a2144
himanshuraj-glitches <himanshuraj9194@gmail.com>
2021-07-02 14:32:02 +0530

    Himanshu Raj

--- ESP8266_Sketch/ESP8266_Sketch.ino
+++ ESP8266_Sketch/ESP8266_Sketch.ino
@@ -1,39 +1,87 @@
 
 #include <ESP8266WiFi.h>
-
-const char* ssid     = "iot";
-const char* password = "project1234";
-
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+// WiFi parameters
+#define WLAN_SSID       "iot"
+#define WLAN_PASS       "project1234"
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME  "Himanshu00"
+#define AIO_KEY       "aio_TFLA24AVuPb5vBkyhY7wBF2nnNHS"
+WiFiClient client;
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish Speed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/speed");
 
 void setup() {
   Serial.begin(115200);
-  delay(100);
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
+  Serial.println(F("Adafruit IO Speed TEST "));
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  delay(10);
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
-  // ------------------------------------------
+    Serial.print(F("."));
   }
-
+  Serial.println();
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.print(WiFi.localIP());
   Serial.println("");
-  Serial.println("WiFi connected");
-  pinMode(LED_BUILTIN,OUTPUT);
-  digitalWrite(LED_BUILTIN,LOW);  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+
+
+  // connect to adafruit io
+  connect();
+
+}
+
+// connect to adafruit io via MQTT
+void connect() {
+  Serial.print(F("Connecting to Adafruit IO... "));
+  int8_t ret;
+  while ((ret = mqtt.connect()) != 0) {
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if (ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(10000);
+  }
+  Serial.println(F("Adafruit IO Connected!"));
 }
 
 void loop() {
+  // ping adafruit io a few times to make sure we remain connected
+  if (! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if (! mqtt.connected())
+      connect();
+  }
 
-  Serial.println("Hello i Am Conected to internet");
-  delay(2000);
+int  temp= random(1,100);
+  Serial.print((int)temp); Serial.print(" KMPH ");
+  Serial.println("");
+  delay(2002);
+
+  if (! Speed.publish(temp)) {                     //Publish to Adafruit
+    Serial.println(F("Failed"));
+  }
+
+  else {
+    Serial.println(F("Sent!"));
+  }
 }
